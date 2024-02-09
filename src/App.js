856033774aa3d16fc7f177b729900ce401099869
Ngoualem Ariel <114417347+Neressoffi@users.noreply.github.com>
2024-02-09 17:11:26 +0100

    Premier commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="ellipse-2"></div>
+      <div className="rectangle-6"></div>
+      <div className="rectangle-1">uuuu</div>
+      <div className="rectangle-7">tttt</div>
+      <div className="line-1">ggggg</div>
+      <div className="citation">CITATION ET ANNIVERSAIRE</div>
+      <div className="quote">I've missed more than 9,000 shots in my career. I've lost almost 300 games. Twenty-six times I've been trusted to take the game-winning shot and missed. I've failed over and over and over again in my life. And that is why I succeed.</div>
+      <div className="michael-jordan">- MICHAEL JORDAN</div>
+    </div>
+  );
+}
+
+export default App;

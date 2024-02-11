b0335d7564234322e0d28105ad71c167289e8b7a
Ngoualem Ariel <114417347+Neressoffi@users.noreply.github.com>
2024-02-11 20:33:11 +0100

    Deuxieme Modif

--- src/components/BirthdayPage.jsx
+++ src/components/BirthdayPage.jsx
@@ -1,12 +1,4 @@
-import React from 'react'
-import Header from './Header/Header.jsx'
-import Hero from './Hero/Hero.jsx'
 
-function BirthdayPage() {
-  return (
-    <div>
-        <Header />
-        <Hero />
     </div>
   )
 }
